84b44a1f518adc7251aa883d5c5c02a1aee5f869
kelvintaywl <kelvintaywl@gmail.com>
2016-08-19 01:55:17 +0900

    add server

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn webhooks.server:app
---
+++ webhooks/server.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+import json
+import os
+
+import requests
+import flask
+
+app = flask.Flask(__name__)
+
+
+app.config.update(dict(
+    DEBUG=True
+))
+
+GITHUB_OWNER = 'kelvintaywl'
+GITHUB_REPO = 'pull_requests'
+GITHUB_API_BASE = 'https://api.github.com'
+
+
+class Github(object):
+
+    def __init__(self, api_key, api_secret, owner, repo):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.owner = owner
+        self.repo = repo
+
+    def make(self, path, method='get', params=None, data=None):
+        url = "{base}/repos/{owner}/{repo}/{path}".format(
+            base=GITHUB_API_BASE,
+            owner=GITHUB_OWNER,
+            repo=GITHUB_REPO,
+            path=path)
+        return requests.request(method, url,
+                                params=params, data=data,
+                                auth=(self.api_key, self.api_secret)
+                                ).json()
+
+    def get_pull_request(self, id):
+        return self.make(
+            'pulls/{id}'.format(
+                id=id
+            ),
+            method='get'
+        )
+
+    def update_pull_request(self, id, title, body, state='open'):
+        data = {
+            'title': title,
+            'body': body,
+            'state': state
+        }
+        return self.make(
+            'pulls/{id}'.format(
+                id=id
+            ),
+            method='patch',
+            data=json.dumps(data)
+        )
+
+
+
+def _handle_github_event(payload):
+    if payload.get('action') == 'opened':
+        id = payload.get('number')
+        g = Github(
+            os.getenv('GITHUB_USERNAME'),
+            os.getenv('GITHUB_TOKEN')
+        )
+        pr = g.get_pull_request(id)
+        print(pr)
+        return None
+
+    return ValueError('unable to process action from Github hook')
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/github/payload', methods=['POST'])
+def github_payload():
+    payload = flask.request.get_json(
+        force=True,
+        silent=True,
+        cache=False)
+
+    if not payload:
+        flask.abort(400)
+
+    err = _handle_github_event(payload)
+    if err:
+        flask.abort(500)
+
+    return  # empty 200 OK
