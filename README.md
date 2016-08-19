4ac5e933fb25fb92f787b4a548321c819b38e5f5
kelvintaywl <kelvintaywl@gmail.com>
2016-08-19 16:50:08 +0900

    add documentation

--- README.md
+++ README.md
@@ -9,3 +9,38 @@ This is more of a concept or prototype to explore how we can give better
 structures to pull requests in Github without making documentation on pull
 requests more painful (but fun and rewarding perhaps).
 
+## Usage
+
+### Templates
+
+It's always good to set a default pull request template for your repository.
+
+For me and my current workflow, there's a standard template i try to follow.
+
+You can view the [template here](.github/PULL_REQUEST_TEMPLATE.md)
+
+### Validating Pull Request descriptions
+
+You can set up a server to validate pull request descriptions based on certain
+rules.
+
+In this repo, i have created a Flask application that listens to pull request
+events from GitHub.
+
+See the [code here](webhooks/server.py) for more information.
+
+To use this sample server, simply fork it or so, and deploy it via Heroku.
+
+#### steps to deploy on Heroku
+
+1. go to your Heroku account and create a new application
+2. link your forked repository.
+3. set `GITHUB_USERNAME` and `GITHUB_TOKEN` enviornment variables in Heroku dashboard with your GitHub handler and access token.
+4. optionally, you can set a `GITHUB_IGNORE_LABEL` environment variable. When a pull request has a label of that value, this server ignores validation
+
+## Todo
+
+- use OAuth client instead of specific GitHub user
+- include PR rule coverage
+- customizing of rule weightage for coverage
+- add more rules (optional)
--- webhooks/server.py
+++ webhooks/server.py
@@ -1,5 +1,39 @@
 # -*- coding: utf-8 -*-
 """
+    Pull Request Bot
+
+    This server listens to webhooks from Github such that
+    when you create / update a pull request, it checks the description,
+    such that:
+
+
+    When opening a new pull request:
+
+    1. if branch name has a story ticket to Pivotal where its name follow:
+    '[ticket ID]-test-branch', it prefixes a link to the description:
+        ```story: https://pivotaltracker.com/story/show/[ticket ID]```
+
+
+    When editing a pull request:
+
+
+    1. if description of pull request meets current rules, it posts a LGTM
+    comment (update `webhooks/static/good_comment.txt` for content).
+    Else, we post a comment signalling the issues
+    (see `webhooks/static/issues.txt`).
+
+    If you prefer to ignore all rules for the pull request,
+    you can simply add a `pr_ignore` label to the pull request.
+    That value can be modified if you set an environment variable
+    `GITHUB_IGNORE_LABEL` with the specific label name.
+
+
+    NOTE:
+
+    Please set `GITHUB_USERNAME` and `GITHUB_TOKEN` environment variables
+    in the server. It would use these values to authenticate for GitHub API
+    requests.
+
 """
 import json
 import os
@@ -21,27 +55,63 @@ GITHUB_OWNER = 'kelvintaywl'
 GITHUB_REPO = 'pull_requests'
 GITHUB_API_BASE = 'https://api.github.com'
 
+# label name to detect if we should ignore rules when validating
+# pull request description; else we would apply all rules @ PR_RULES
 GITHUB_IGNORE_LABEL = os.getenv('GITHUB_IGNORE_LABEL', 'pr_ignore')
 
 
 class Rule(object):
+    """
+    Rule class that defines what makes a good pull request description.
 
+    It applies validation on description with its `validate` method.
+
+    Note:
+
+        self.fn should be a function that returns True if the provided
+        text meets the rule criteria.
+
+    Example:
+
+        # we create a Rule instance to check that all lines start with 'The'
+        >>> empty_line_rule = Rule(
+        >>>    "all lines start with 'The'", all, lambda x: x.startswith('The')
+        >>>>)
+
+    Args:
+        desc (str): Human readable string describing the rule (imperative)
+        quantifier (:obj:`function`): should be `any` or `all`
+        fn (:obj:`function`): function to validate if text meets rule
+
+    Attributes:
+        desc (str): Human readable string describing the rule (imperative)
+        quantifier (:obj:`function`): should be `any` or `all`
+        fn (:obj:`function`): function to validate if text meets rule
+
+    """
     def __init__(self, desc, quantifier, fn):
         self.desc = desc
-        self.quantifier = quantifier
-        self.fn = fn
-
-    def __repr__(self):
-        return self.desc, self.quantifier, self.fn
+        self.quantifier = quantifier  # should be `any` or `all`
+        self.fn = fn  # rule function
 
     def validate(self, lines):
+        """
+        Validate that either one of the lines or all of the lines meet the
+        rule.
+
+        Whether it is just one of all of the lines,
+        it depends on the value of self.quantifier
+
+        Returns:
+            str: description of rule if violated, else None
+        """
         if self.quantifier([self.fn(line) for line in lines]):
             # rule is satisfied
             return None
         # else, we return the description of the rule violated
         return self.desc
 
-
+# current list of all applicable rules
 PR_RULES = {
     'story': Rule(
         "should have story link",
@@ -57,6 +127,12 @@ PR_RULES = {
 
 
 class Github(object):
+    """
+    Github API client class to make API calls on a specific repository
+    of a certain owner.
+
+    Only certain endpoints are implemented / supported.
+    """
 
     def __init__(self, api_key, api_secret, owner, repo):
         self.api_key = api_key
@@ -64,11 +140,25 @@ class Github(object):
         self.owner = owner
         self.repo = repo
 
-    def make(self, path, method='get', params=None, data=None):
+    def make(self, path, method='get',
+             owner=GITHUB_OWNER, repo=GITHUB_REPO,
+             params=None, data=None):
+        """
+        make API call to Github API.
+
+        Args:
+            path (str): API sub path
+            method (str): lowercase HTTP method (e.g., `get`, `post`)
+            owner (str): name of user/organization on GitHub
+            repo (str): name of repository on GitHub
+            params (dict): for GET parameters (querystring)
+            data (dict): data paylod for POST requests
+        """
+
         url = "{base}/repos/{owner}/{repo}/{path}".format(
             base=GITHUB_API_BASE,
-            owner=GITHUB_OWNER,
-            repo=GITHUB_REPO,
+            owner=owner,
+            repo=repo,
             path=path)
         return requests.request(method, url,
                                 params=params, data=data,
@@ -118,13 +208,16 @@ class Github(object):
         )
 
 
-def _prefix_story_link_in_pull_request(id):
+def _prefix_story_link_in_pull_request(owner, repo, id):
+    """
+
+    """
     g = Github(
-                os.getenv('GITHUB_USERNAME'),
-                os.getenv('GITHUB_TOKEN'),
-                GITHUB_OWNER,
-                GITHUB_REPO
-            )
+        os.getenv('GITHUB_USERNAME'),
+        os.getenv('GITHUB_TOKEN'),
+        owner,
+        repo
+    )
     pr = g.get_pull_request(id)
 
     title = pr['title']
@@ -148,17 +241,20 @@ def _qualify_description(description, ignore_list=[]):
 
     Args:
         description (str): Pull request description
+        ignore_list (list[str]): list of rule names to ignore. See PR_RULES
 
     Returns:
         bool: True if description meets criterias, else False
         list[str]: list of criterias not met, else None
     """
     lines = description.split('\n')
+
+    # leave out specific rules if specified
     rules = PR_RULES.copy()
     for rule_name in ignore_list:
         rules.pop(rule_name)
 
-
+    # grab all issues (which rules were violated)
     issues = [
         i for i in _yield_rule_adherence(rules.values(), lines)
         if i is not None
@@ -166,21 +262,22 @@ def _qualify_description(description, ignore_list=[]):
     return bool(not any(issues)), issues
 
 
-def _validate_pull_request_description(id):
+def _validate_pull_request_description(owner, repo, id):
     """
-    Validate Pull request's description, and makes comment on the Pull
-    request on quality.
+    Validate Pull request's description, and makes comment on the pull
+    request on quality. It sends a comment thereafter on the pull request
+    to describe the quality of the pull request description
 
     Args:
         id (int): Pull request ID
     """
 
     g = Github(
-                os.getenv('GITHUB_USERNAME'),
-                os.getenv('GITHUB_TOKEN'),
-                GITHUB_OWNER,
-                GITHUB_REPO
-            )
+        os.getenv('GITHUB_USERNAME'),
+        os.getenv('GITHUB_TOKEN'),
+        owner,
+        repo
+    )
     pr = g.get_pull_request(id)
 
     body = pr['body']
@@ -196,17 +293,25 @@ def _validate_pull_request_description(id):
                 os.path.join(STATIC_DIR, 'good_comment.txt')
         ) as txt:
             comment = txt.read()
+
             g.comment_on_pull_request(id, comment)
     else:
         with open(
             os.path.join(STATIC_DIR, 'issues.txt')
         ) as txt:
+            # format issues into list items
             issues_printed = '\n- '.join([''] + issues)
             comment = txt.read().format(issues=issues_printed)
+
             g.comment_on_pull_request(id, comment)
 
 
 def _handle_github_pull_request_event(payload):
+    owner, repo = GITHUB_OWNER, GITHUB_REPO
+    if payload.get('repo'):
+        repo_name = payload['repo']['full_name']
+        owner, repo = repo_name.split('/', 1)[:2]
+
     if payload.get('zen'):
         # status check
         print('ping pong')
@@ -217,10 +322,10 @@ def _handle_github_pull_request_event(payload):
 
     if action:
         if action in ('opened', 'reopened'):
-            _prefix_story_link_in_pull_request(id)
+            _prefix_story_link_in_pull_request(owner, repo, id)
 
         if action == 'edited':
-            _validate_pull_request_description(id)
+            _validate_pull_request_description(owner, repo, id)
 
         return None
 
@@ -229,11 +334,22 @@ def _handle_github_pull_request_event(payload):
 
 @app.route('/')
 def index():
+    """
+    home page
+
+    TODO: make it shiny
+    """
     return flask.render_template('index.html')
 
 
 @app.route('/github/payload', methods=['POST'])
 def github_payload():
+    """
+    Callback endpoint for GitHub webhooks (POST)
+
+    Currently, only supports for pull request events. Please see:
+    https://developer.github.com/v3/activity/events/types/#pullrequestevent
+    """
     payload = flask.request.get_json(
         force=True,
         silent=True,
@@ -246,4 +362,4 @@ def github_payload():
     if err:
         flask.abort(500)
 
-    return 'ok'
+    return 'beep boop'
